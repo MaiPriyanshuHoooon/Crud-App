980058a6e7bfb56476a8bae47f5cb3e0a1ccb113
Priyanshu Malik <mallikpriyanshu03@gmail.com>
2025-11-23 14:38:27 +0530

    feat: prepare app for docker deployment

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -5,10 +5,17 @@ const morgan = require("morgan");
 
 
 app.use(cors("*"));
-app.use(morgan('dev')); 
+app.use(morgan('dev'));
 app.use(express.json({}));
 app.use(express.urlencoded({ extended: false }));
 
 app.use("/api/v1", require("./routes"));
+
+app.use(express.static(path.join(__dirname, '../dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../dist/index.html'));
+});
+
 
 module.exports = app;
--- Frontend/src/utils/axiosClient.js
+++ Frontend/src/utils/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const axiosClient = axios.create({
-    baseURL: 'http://localhost:4500/api/v1'
+    baseURL: '/api/v1'
 });
 
 
